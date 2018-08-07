e6d4264a8d7d3f5b198028d83f402a51cd73e164
nikopit <niko.pitkonen@helsinki.fi>
2018-08-07 10:36:04 +0300

    yhteys mongoon

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/tododb'
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+MongoClient.connect(url, {useNewUrlParser: true}, (err, db) => {
+    if (err != null) {
+        console.error("Virhe yhteyden avaamisessa!" + err.message);
+        throw err;
+    }
+    const dbo = db.db();
+    console.log("Yhteys Mongoon saatu");
+    const kokoelma = dbo.collection('list');
+
 });
 
 module.exports = router;
