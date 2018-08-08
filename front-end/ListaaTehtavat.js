638c8f85f184459f332cf5d6a41fe984addeec94
kaarinasuom <kaarina.suominen@outlook.com>
2018-08-08 16:39:25 +0300

    post ja delete toimii client päässä

--- front-end/ListaaTehtavat.js
+++ front-end/ListaaTehtavat.js
@@ -1,91 +1,131 @@
 
-// luodaan uusi listaelementti, aina kun klikataan lisäys nappulaa
-function uusiElementti() {
-        var XHR = new XMLHttpRequest();
-        // Define what happens on successful data submission
-        XHR.addEventListener('load', function(event) {
-            alert('Yeah! Data sent and response loaded.');
-            });
-        var lahetettavadata = {
+// funktio lähettää datan mongoon aina kun lisätään uusi tehtävö
+function uusiTehtava() {
+    var XHR = new XMLHttpRequest();
+    // Define what happens on successful data submission
+    XHR.addEventListener('load', function (event) {
+        alert('Yeah! Data sent and response loaded.');
+    });
+    var lahetettavadata = {
         title: document.getElementById('myInput').value,
         content: 'placeholder'
     }
-    XHR.onreadystatechange = function() {
-    console.log("DBG", XHR.readyState);
-    if (XHR.readyState===4) {
-        console.log(XHR.statusText);
-        if(XHR.status === 200) {
-            console.log("vastaus", XHR.responseText);
+
+    XHR.onreadystatechange = function () {
+        console.log("DBG", XHR.readyState);
+        if (XHR.readyState === 4) {
+            console.log(XHR.statusText);
+            if (XHR.status === 200) {
+                console.log("vastaus", XHR.responseText);
+                newElement(JSON.parse(XHR.responseText));
+            }
         }
     }
-}
-        // Define what happens in case of error
-        XHR.addEventListener('error', function(event) {
-            alert('Oops! Something goes wrong.');
-        });
-        // Set up our request
-        XHR.open('POST', 'http://localhost:3000/notes');
+    // Define what happens in case of error
+    XHR.addEventListener('error', function (event) {
+        alert('Oops! Something goes wrong.');
+    });
+    // Set up our request
+    XHR.open('POST', 'http://localhost:3000/notes');
     // Add the required HTTP header for form data POST requests
     XHR.setRequestHeader('Content-Type', 'application/json');
     // Finally, send our data.
-        XHR.send(JSON.stringify(lahetettavadata));
+    XHR.send(JSON.stringify(lahetettavadata));
+
+
+}
+
+// lisää HTML sivulle uuden tehtäväpalkin
+function newElement(uusi) {
+    var li = document.createElement("li");
+    var inputValue = uusi.title;
+    var t = document.createTextNode(inputValue);
+    var myUL = document.getElementById("myUL");
+    myUL.appendChild(li);
+    li.appendChild(t);
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    var tieto = document.createElement("input")
+    tieto.setAttribute("type", "hidden");
+    tieto.value = uusi._id;
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+        close.onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+
+    span.onclick = function () {
+        poistaTehtava(uusi._id);
+        myUL.removeChild(li);
+    }
 
 }
 
 
+/////////////////////////////////////
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+
+
+function poistaTehtava(_id) {
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function () {
+        console.log("DBG", XHR.readyState);
+        if (XHR.readyState === 4) {
+            console.log(XHR.statusText);
+            if (XHR.status === 200) {
+                console.log("vastaus", XHR.responseText);
+            }
+        }
+    }
+    XHR.addEventListener('error', function (event) {
+        alert('Oops! Something goes wrong.');
+    });
+    XHR.open('DELETE', 'http://localhost:3000/notes/' + _id);
+    XHR.setRequestHeader('Content-Type', 'application/json');
+    XHR.send();
+}
+
+
+// Luodaan "poista" button and append it to each list item
+var lista = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (i = 0; i < lista.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    lista[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
         var div = this.parentElement;
         div.style.display = "none";
+        poistaTehtava(poistettavadata);
+
     }
 }
+
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
+list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
         }
-    }
-}
+    },
+    false);
+
+
+//////////////////////////
+
+
